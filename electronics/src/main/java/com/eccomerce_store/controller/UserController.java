44bb41aee826746610ad10dceffcf1fbafda222d
benylin153 <absithole153@gmail.com>
2026-09-14 21:58:28 +0200

    Refactor UserController to return user profile
    Refactor UserController to include UserRepository and return user profile information.

--- electronics/src/main/java/com/eccomerce_store/controller/UserController.java
+++ electronics/src/main/java/com/eccomerce_store/controller/UserController.java
@@ -1,16 +1,36 @@
 package com.eccomerce_store.controller;
+
+import com.eccomerce_store.dto.UserProfileResponse;
+import com.eccomerce_store.electronics.User;
+import com.eccomerce_store.repository.UserRepository;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/user")
-public class UserController {
-    @GetMapping("/profile")//may change
-    public String profile(
-            Authentication authentication) {
+@CrossOrigin
+public class UserController 
+{
+    private final UserRepository userRepository;
 
-        return "Welcome "
-                + authentication.getName()
-                + "! You are logged in.";
+    public UserController(UserRepository userRepository)
+    {
+        this.userRepository = userRepository;
+    }
+
+    @GetMapping("/profile")
+    public ResponseEntity<?> profile(Authentication authentication)
+    {
+        // Gets the username of the currently logged-in user
+        String username = authentication.getName();
+
+        // finds that user in the database
+        User user = userRepository.findByUsername(username).orElseThrow(() -> new RuntimeException("User not found"));
+
+        // Return user information without the password
+        UserProfileResponse response = new UserProfileResponse(user.getId(), user.getUsername(), user.getEmail(), user.getFirstName(), user.getLastName(), user.getRole(), user.getGender(), user.getDob());
+
+        return ResponseEntity.ok(response);
     }
 }
