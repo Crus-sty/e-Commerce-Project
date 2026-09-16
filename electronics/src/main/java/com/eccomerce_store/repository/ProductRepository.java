115e15875a0f6ac54788ad5ead0b35dc1312bcb9
benylin153 <absithole153@gmail.com>
2026-09-16 18:21:40 +0200

    Enhance ProductRepository with search functionality
    Added search methods for products with sorting options.

--- electronics/src/main/java/com/eccomerce_store/repository/ProductRepository.java
+++ electronics/src/main/java/com/eccomerce_store/repository/ProductRepository.java
@@ -6,7 +6,8 @@ import org.springframework.stereotype.Repository;
 
 import java.util.List;
 @Repository
-public interface ProductRepository extends JpaRepository<Product, Long> {
+public interface ProductRepository extends JpaRepository<Product, Long>
+    {
     // Find products by category
 
     List<Product> findByCategoryIgnoreCase(String category);
@@ -27,4 +28,43 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
     List<Product> findAllByOrderByPriceAsc();
 
     List<Product> findAllByOrderByPriceDesc();
+
+         @Query("""
+        SELECT p
+        FROM Product p
+        WHERE LOWER(p.name) LIKE LOWER(CONCAT('%', :keyword, '%'))
+           OR LOWER(p.description) LIKE LOWER(CONCAT('%', :keyword, '%'))
+    """)
+    List<Product> searchProducts(@Param("keyword") String keyword);
+
+    // Search products and sort from cheapest to most expensive
+    @Query("""
+        SELECT p
+        FROM Product p
+        WHERE LOWER(p.name) LIKE LOWER(CONCAT('%', :keyword, '%'))
+           OR LOWER(p.description) LIKE LOWER(CONCAT('%', :keyword, '%'))
+        ORDER BY p.price ASC
+    """)
+    List<Product> searchProductsPriceAsc(@Param("keyword") String keyword);
+
+    // Search products and sort from most expensive to cheapest
+    @Query("""
+        SELECT p
+        FROM Product p
+        WHERE LOWER(p.name) LIKE LOWER(CONCAT('%', :keyword, '%'))
+           OR LOWER(p.description) LIKE LOWER(CONCAT('%', :keyword, '%'))
+        ORDER BY p.price DESC
+    """)
+    List<Product> searchProductsPriceDesc(@Param("keyword") String keyword);
+
+
+    // Search products and sort alphabetically by name
+    @Query("""
+        SELECT p
+        FROM Product p
+        WHERE LOWER(p.name) LIKE LOWER(CONCAT('%', :keyword, '%'))
+           OR LOWER(p.description) LIKE LOWER(CONCAT('%', :keyword, '%'))
+        ORDER BY p.name ASC
+    """)
+    List<Product> searchProductsNameAsc(@Param("keyword") String keyword);
 }
